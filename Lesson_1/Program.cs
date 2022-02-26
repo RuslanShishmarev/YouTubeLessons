680a482d6a4a393f87af481d56411140d8b636b8
Ruslan Shishmarev <mr.shruslan@gmail.com>
2022-02-27 00:20:30 +0300

    Урок 1 и 2

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputByUser = Console.ReadLine();
+            string inputByUserLower = inputByUser.ToLower();
+
+            bool isStrHello = inputByUserLower.Contains("привет");
+
+            if (isStrHello == true) Console.WriteLine("И тебе привет!");
+
+            Console.WriteLine("Пользователь ввел: " + inputByUser);
+        }
+    }
+}
---
+++ Lesson_2_CheckBadWords/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Lesson_2_CheckBadWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] badWords = new string[] { "тупой", "тупая", "тупые", "урод"};
+
+            string textByUser = Console.ReadLine();
+
+            string[] textAsArray = textByUser.Split(' ');
+
+            foreach(string word in textAsArray)
+            {
+                if(word.Length > 2)
+                {
+                    foreach(string badW in badWords)
+                    {
+                        if(word.ToLower().Contains(badW))
+                        {
+                            Console.WriteLine("Ваше предложение не прошло проверку. Вы нарушили правило сообщества. Подозрительное слово: " + word);
+                            return;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Конец программы");
+        }
+    }
+}
