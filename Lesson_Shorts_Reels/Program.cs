feab96fbff2caaa5f6929311f9dd66c631b0b535
Ruslan Shishmarev <mr.shruslan@gmail.com>
2023-11-08 16:38:00 +0300

    added new ex

---
+++ Lesson_Shorts_Reels/Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Перевернуть каждое слово в предложении,
+//сохранив порядок без использования сторонних библиотек
+//съешь же ещё этих мягких французских булок, да выпей чаю
+
+string input = "съешь же ещё этих мягких французских булок, да выпей чаю";
+
+var reverse = (string text) =>
+{
+    string result = string.Empty;
+
+	for (int i = text.Length - 1; i >= 0; i--)
+	{
+        result += text[i];
+    }
+
+    return result;
+};
+
+var splitText = (string text) =>
+{
+    text += " ";
+    string wordHelp = string.Empty;
+    var result = new List<string>();
+
+    for (int i = 0; i < text.Length; i++)
+    {
+        wordHelp += text[i];
+        if (text[i] == ' ')
+        {
+            result.Add(wordHelp);
+            wordHelp = string.Empty;
+        }
+    }
+
+    return result;
+};
+
+string resultProg = string.Empty;
+
+var textAsArray = splitText(input);
+
+foreach (string word in textAsArray)
+{
+    resultProg += reverse(word); 
+}
+
+
+Console.WriteLine(resultProg);
---
+++ Lessons_9_UnpackString/Program.cs
@@ -0,0 +1,102 @@
+﻿using System.Text;
+
+/*Рассмотрим простой алгоритм сжатия строки. 
+Если в строке есть несколько подряд идущих одинаковых подстрок, можно заменить их на группу. 
+Например, строку aabaabaab можно записать как 3(aab). 
+Можно алгоритм применить к сжатой строке, получив вложенные группы: 3(2ab). 
+Дана сжатая строка, требуется её распаковать. 
+Например, для строки a2(a2(bc))3db ответ будет aabcbcabcbcdddb.*/
+
+string input1 = "a2(a2(bc))3db";
+string check1 = "aabcbcabcbcdddb";
+
+string input2 = "aa2(bc)db";
+string check2 = "aabcbcdb";
+
+string input3 = "4w2o3(3(q))";
+string check3 = "wwwwooqqqqqqqqq";
+
+var result1 = GetUnpackStr(new StringBuilder(input1));
+var result2 = GetUnpackStr(new StringBuilder(input2));
+var result3 = GetUnpackStr(new StringBuilder(input3));
+
+Console.WriteLine(result1);
+Console.WriteLine(check1 == result1.ToString());
+
+Console.WriteLine(result2);
+Console.WriteLine(check2 == result2.ToString());
+
+Console.WriteLine(result3);
+Console.WriteLine(check3 == result3.ToString());
+
+StringBuilder GetUnpackStr(StringBuilder input)
+{
+    var resultStr = new StringBuilder();
+    for (int i = 0; i < input.Length; i++)
+    {
+        char sb = input[i];
+
+        int sbAsInt;
+        if (int.TryParse(sb.ToString(), out sbAsInt))
+        {
+            StringBuilder result = new StringBuilder();
+
+            char nextSb = input[i + 1];
+            if (nextSb == '(')
+            {
+                var forBrace = input.ToString()[(i + 1)..(input.Length - 1)];
+                var innerBrace = GetInnerBraceStr(forBrace);
+
+                result = GetUnpackStr(innerBrace);
+
+                i += innerBrace.Length + 2;
+            }
+            else
+            {
+                result = result.Append(nextSb);
+                i++;
+            }
+            for (int charCount = 0; charCount < sbAsInt; charCount++)
+            {
+                resultStr.Append(result);
+            }
+        }
+        else
+        {
+            resultStr.Append(sb);
+        }
+    }
+
+    return resultStr;
+}
+
+StringBuilder GetInnerBraceStr(string input)
+{
+    var resultStr = new StringBuilder();
+    int openCount = 1;
+    int closeCount = 0;
+    for (int i = 1; i < input.Length; i++)
+    {
+        char sb = input[i];
+
+        if (sb == '(')
+        {
+            openCount++;
+        }
+        if (sb == ')')
+        {
+            closeCount++;
+        }
+
+        if (closeCount < openCount)
+        {
+            resultStr.Append(sb);
+        }
+
+        if (openCount == closeCount)
+        {
+            return resultStr;
+        }
+    }
+    return resultStr;
+}
